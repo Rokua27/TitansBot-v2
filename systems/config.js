56592658171f759d5b692cfdc9025c60174f7eba
Rokua27 <davidrr2710@gmail.com>
2026-07-13 21:49:57 -0500

    Add configuration settings for TitansBot V2

--- systems/config.js
+++ systems/config.js
@@ -1,1 +1,29 @@
+module.exports = {
 
+    nombreBot: "TitansBot V2",
+
+    version: "2.0.0",
+
+    prefijo: "/",
+
+    spamMensajes: 15,
+
+    tiempoSpam: 10000,
+
+    maxAdvertencias: 10,
+
+    advertenciasSilencio: 5,
+
+    advertenciasCriticas: 15,
+
+    silencioAutomaticoMinutos: 30,
+
+    silencioGraveHoras: 24,
+
+    comunidad: "Liga Titans Team",
+
+    directores: [
+        "Jean Pierre Rousseau",
+        "David Rivera"
+    ]
+}
