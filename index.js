5b9eef4eee25f4d543e9f1e03b2c99d390b5908b
Rokua27 <davidrr2710@gmail.com>
2026-07-13 21:56:50 -0500

    Add bot configuration logging to index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("TitansBot V2 iniciado correctamente")
+const config = require("./system/config")
+
+const {
+    cargarJSON,
+    guardarJSON
+} = require("./utils/utils")
+
+console.log("================================")
+console.log(`🤖 ${config.nombreBot}`)
+console.log(`📦 Versión: ${config.version}`)
+console.log(`🏆 Comunidad: ${config.comunidad}`)
+console.log("================================")
