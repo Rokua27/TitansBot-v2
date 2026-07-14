3feb334fc8fae8b25a45649403edfecb5f98e227
Rokua27 <davidrr2710@gmail.com>
2026-07-13 21:08:13 -0500

    Implement role management functions in roles.js

--- systems/roles.js
+++ systems/roles.js
@@ -1,1 +1,29 @@
+const {
+    cargarJSON,
+    guardarJSON
+} = require("./config")
 
+const archivo = "./data/roles.json"
+
+let roles = cargarJSON(archivo)
+
+function obtenerRol(usuario) {
+    return roles[usuario] || "usuario"
+}
+
+function asignarRol(usuario, rol) {
+    roles[usuario] = rol
+    guardarJSON(archivo, roles)
+}
+
+function eliminarRol(usuario) {
+    delete roles[usuario]
+    guardarJSON(archivo, roles)
+}
+
+module.exports = {
+    obtenerRol,
+    asignarRol,
+    eliminarRol,
+    roles
+}
