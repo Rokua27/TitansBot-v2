e19cb1255ae7f65463bcd8e13c9e5936090f9e18
Rokua27 <davidrr2710@gmail.com>
2026-07-14 21:37:34 -0500

    Add mute command to silence users

---
+++ commands/moderacion/mute.js
@@ -0,0 +1,61 @@
+const {
+    cargarJSON,
+    guardarJSON
+} = require("../../utils/utils")
+
+module.exports = {
+
+    nombre: "mute",
+
+    ejecutar: async (
+        sock,
+        mensaje,
+        args
+    ) => {
+
+        const usuario =
+            mensaje.message.extendedTextMessage
+            ?.contextInfo
+            ?.mentionedJid?.[0]
+
+        if (!usuario) {
+
+            return await sock.sendMessage(
+                mensaje.key.remoteJid,
+                {
+                    text:
+                        "❌ Debes mencionar un usuario.\n\nEjemplo:\n/mute @usuario"
+                }
+            )
+        }
+
+        const silenciados =
+            cargarJSON(
+                "./data/silenciados.json",
+                {}
+            )
+
+        silenciados[usuario] = {
+
+            fecha: Date.now(),
+            moderador: mensaje.key.participant
+        }
+
+        guardarJSON(
+            "./data/silenciados.json",
+            silenciados
+        )
+
+        await sock.sendMessage(
+            mensaje.key.remoteJid,
+            {
+                text:
+`🔇 Usuario silenciado
+
+👤 Usuario:
+@${usuario.split("@")[0]}`,
+                mentions: [usuario]
+            }
+        )
+    }
+}
