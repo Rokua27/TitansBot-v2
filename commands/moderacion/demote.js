38f28038dd9f57a34913ea3f2cdfefe07ae364b2
Rokua27 <davidrr2710@gmail.com>
2026-07-14 21:53:51 -0500

    Add demote command to remove user from admin

---
+++ commands/moderacion/demote.js
@@ -0,0 +1,62 @@
+module.exports = {
+
+    nombre: "demote",
+
+    ejecutar: async (
+        sock,
+        mensaje,
+        args
+    ) => {
+
+        const grupo =
+            mensaje.key.remoteJid
+
+        const usuario =
+            mensaje.message.extendedTextMessage
+            ?.contextInfo
+            ?.mentionedJid?.[0]
+
+        if (!usuario) {
+
+            return await sock.sendMessage(
+                grupo,
+                {
+                    text:
+                        "❌ Debes mencionar un usuario."
+                }
+            )
+        }
+
+        try {
+
+            await sock.groupParticipantsUpdate(
+                grupo,
+                [usuario],
+                "demote"
+            )
+
+            await sock.sendMessage(
+                grupo,
+                {
+                    text:
+`⬇️ Usuario removido de administración
+
+@${usuario.split("@")[0]}`,
+                    mentions: [usuario]
+                }
+            )
+
+        } catch (error) {
+
+            console.log(error)
+
+            await sock.sendMessage(
+                grupo,
+                {
+                    text:
+                        "❌ No pude quitar los permisos."
+                }
+            )
+        }
+    }
+}
