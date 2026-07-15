4a80093c313a6d806a0364c141b055ceee5e6b82
Rokua27 <davidrr2710@gmail.com>
2026-07-14 23:42:16 -0500

    Implement capitan command to assign team captain

---
+++ commands/liga/capitan.js
@@ -0,0 +1,94 @@
+const {
+    cargarJSON,
+    guardarJSON
+} = require("../../utils/utils")
+
+module.exports = {
+
+    nombre: "capitan",
+
+    ejecutar: async (
+        sock,
+        mensaje,
+        args
+    ) => {
+
+        if (args.length < 2) {
+
+            return await sock.sendMessage(
+                mensaje.key.remoteJid,
+                {
+                    text:
+`❌ Uso correcto:
+
+/capitan Kelra Titans_Esports`
+                }
+            )
+        }
+
+        const jugador =
+            args[0]
+
+        const equipo =
+            args.slice(1)
+                .join(" ")
+                .replaceAll("_", " ")
+
+        const liga =
+            cargarJSON(
+                "./data/liga.json",
+                {
+                    jugadores: {},
+                    equipos: {}
+                }
+            )
+
+        if (
+            !liga.jugadores[jugador]
+        ) {
+
+            return await sock.sendMessage(
+                mensaje.key.remoteJid,
+                {
+                    text:
+                        `❌ El jugador ${jugador} no existe.`
+                }
+            )
+        }
+
+        if (
+            liga.jugadores[jugador].equipo !== equipo
+        ) {
+
+            return await sock.sendMessage(
+                mensaje.key.remoteJid,
+                {
+                    text:
+                        `❌ ${jugador} no pertenece a ${equipo}.`
+                }
+            )
+        }
+
+        liga.equipos[equipo].capitan =
+            jugador
+
+        guardarJSON(
+            "./data/liga.json",
+            liga
+        )
+
+        await sock.sendMessage(
+            mensaje.key.remoteJid,
+            {
+                text:
+`👑 Capitán asignado
+
+🏆 Escuadra:
+${equipo}
+
+👤 Capitán:
+${jugador}`
+            }
+        )
+    }
+}
