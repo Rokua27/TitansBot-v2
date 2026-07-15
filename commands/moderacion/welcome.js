108bbf297a7e50615356f0efa9cd76f7767f8791
Rokua27 <davidrr2710@gmail.com>
2026-07-15 01:30:06 -0500

    Add welcome command for group configuration

---
+++ commands/moderacion/welcome.js
@@ -0,0 +1,63 @@
+const {
+    cargarJSON,
+    guardarJSON
+} = require("../../utils/utils")
+
+module.exports = {
+
+    nombre: "welcome",
+
+    ejecutar: async (
+        sock,
+        mensaje,
+        args
+    ) => {
+
+        const config =
+            cargarJSON(
+                "./data/configGrupo.json",
+                {
+                    welcome: true,
+                    bye: true
+                }
+            )
+
+        const opcion =
+            args[0]?.toLowerCase()
+
+        if (
+            opcion !== "on" &&
+            opcion !== "off"
+        ) {
+
+            return await sock.sendMessage(
+                mensaje.key.remoteJid,
+                {
+                    text:
+`Uso correcto:
+
+/welcome on
+/welcome off`
+                }
+            )
+        }
+
+        config.welcome =
+            opcion === "on"
+
+        guardarJSON(
+            "./data/configGrupo.json",
+            config
+        )
+
+        await sock.sendMessage(
+            mensaje.key.remoteJid,
+            {
+                text:
+                    config.welcome
+                    ? "✅ Bienvenidas activadas."
+                    : "❌ Bienvenidas desactivadas."
+            }
+        )
+    }
+}
