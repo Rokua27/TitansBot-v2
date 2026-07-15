8af1aee67e3f4f7857fb4c4f6919c3b42176c1fa
Rokua27 <davidrr2710@gmail.com>
2026-07-15 01:31:16 -0500

    Add 'bye' command for managing farewell messages

---
+++ commands/moderacion/bye.js
@@ -0,0 +1,63 @@
+const {
+    cargarJSON,
+    guardarJSON
+} = require("../../utils/utils")
+
+module.exports = {
+
+    nombre: "bye",
+
+    ejecutar: async (
+        sock,
+        mensaje,
+        args
+    ) => {
+
+        const config =
+            cargarJSON(
+                "./data/configGrupo.json",
+                {
+                    welcome: true,
+                    bye: true
+                }
+            )
+
+        const opcion =
+            args[0]?.toLowerCase()
+
+        if (
+            opcion !== "on" &&
+            opcion !== "off"
+        ) {
+
+            return await sock.sendMessage(
+                mensaje.key.remoteJid,
+                {
+                    text:
+`Uso correcto:
+
+/bye on
+/bye off`
+                }
+            )
+        }
+
+        config.bye =
+            opcion === "on"
+
+        guardarJSON(
+            "./data/configGrupo.json",
+            config
+        )
+
+        await sock.sendMessage(
+            mensaje.key.remoteJid,
+            {
+                text:
+                    config.bye
+                    ? "✅ Despedidas activadas."
+                    : "❌ Despedidas desactivadas."
+            }
+        )
+    }
+}
