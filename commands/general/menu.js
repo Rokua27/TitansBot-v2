201cfcf416eddfd35e5fea1a8d8f4451b9c4ac9e
Rokua27 <davidrr2710@gmail.com>
2026-07-14 20:34:36 -0500

    Add menu command for TitansBot V2

---
+++ commands/general/menu.js
@@ -0,0 +1,50 @@
+module.exports = {
+
+    nombre: "menu",
+
+    ejecutar: async (
+        sock,
+        msg
+    ) => {
+
+        const menu = `
+🤖 *TitansBot V2*
+
+📌 Comandos disponibles
+
+/general
+• /ping
+• /menu
+
+👮 Moderación
+• /warn
+• /unwarn
+• /silenciar
+• /expulsar
+
+🎭 Roles
+• /rol
+• /quitarrol
+
+⚽ Liga
+• /tabla
+• /partidos
+
+🎮 Diversión
+• /8ball
+• /dado
+• /moneda
+
+📊 Actividad
+• /ranking
+• /stats
+`;
+
+        await sock.sendMessage(
+            msg.key.remoteJid,
+            {
+                text: menu
+            }
+        );
+    }
+}
