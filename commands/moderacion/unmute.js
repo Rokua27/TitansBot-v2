6c8685bfb51e16d1348d52169501cd8d27a707f7
Rokua27 <davidrr2710@gmail.com>
2026-07-14 21:39:00 -0500

    Implement unmute command to reactivate users

---
+++ commands/moderacion/unmute.js
@@ -0,0 +1,57 @@
+const {
+    cargarJSON,
+    guardarJSON
+} = require("../../utils/utils")
+
+module.exports = {
+
+    nombre: "unmute",
+
+    ejecutar: async (
+        sock,
+        mensaje,
+        args
+    ) => {
+
+        const usuario =
+            mensaje.message.extendedTextMessage
+            ?.contextInfo
+            ?.mentionedJid?.[0]
+
+        if (!usuario) {
+
+            return await sock.sendMessage(
+                mensaje.key.remoteJid,
+                {
+                    text:
+                        "❌ Debes mencionar un usuario."
+                }
+            )
+        }
+
+        const silenciados =
+            cargarJSON(
+                "./data/silenciados.json",
+                {}
+            )
+
+        delete silenciados[usuario]
+
+        guardarJSON(
+            "./data/silenciados.json",
+            silenciados
+        )
+
+        await sock.sendMessage(
+            mensaje.key.remoteJid,
+            {
+                text:
+`🔊 Usuario reactivado
+
+👤 Usuario:
+@${usuario.split("@")[0]}`,
+                mentions: [usuario]
+            }
+        )
+    }
+}
