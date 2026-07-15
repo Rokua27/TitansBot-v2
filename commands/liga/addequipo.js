51c534a49fe0e3052132dc1804007269a1ae1754
Rokua27 <davidrr2710@gmail.com>
2026-07-14 22:35:48 -0500

    Add command to register a new team in the league

---
+++ commands/liga/addequipo.js
@@ -0,0 +1,82 @@
+const {
+    cargarJSON,
+    guardarJSON
+} = require("../../utils/utils")
+
+module.exports = {
+
+    nombre: "addequipo",
+
+    ejecutar: async (
+        sock,
+        mensaje,
+        args
+    ) => {
+
+        const nombreEquipo =
+            args.join(" ")
+
+        if (!nombreEquipo) {
+
+            return await sock.sendMessage(
+                mensaje.key.remoteJid,
+                {
+                    text:
+                        "❌ Debes indicar un nombre.\n\nEjemplo:\n/addequipo Titans FC"
+                }
+            )
+        }
+
+        const liga =
+            cargarJSON(
+                "./data/liga.json",
+                {
+                    equipos: {},
+                    goleadores: {},
+                    mvp: {},
+                    partidos: []
+                }
+            )
+
+        if (
+            liga.equipos[nombreEquipo]
+        ) {
+
+            return await sock.sendMessage(
+                mensaje.key.remoteJid,
+                {
+                    text:
+                        "⚠️ Ese equipo ya existe."
+                }
+            )
+        }
+
+        liga.equipos[nombreEquipo] = {
+
+            pj: 0,
+            pg: 0,
+            pe: 0,
+            pp: 0,
+            gf: 0,
+            gc: 0,
+            dg: 0,
+            pts: 0
+        }
+
+        guardarJSON(
+            "./data/liga.json",
+            liga
+        )
+
+        await sock.sendMessage(
+            mensaje.key.remoteJid,
+            {
+                text:
+`✅ Equipo registrado correctamente
+
+🏆 Equipo:
+${nombreEquipo}`
+            }
+        )
+    }
+}
