a3e2b8241eab820168c1131cbb000fa626828c77
Rokua27 <davidrr2710@gmail.com>
2026-07-14 23:40:07 -0500

    Add roster command to display team players

---
+++ commands/liga/roster.js
@@ -0,0 +1,84 @@
+const {
+    cargarJSON
+} = require("../../utils/utils")
+
+module.exports = {
+
+    nombre: "roster",
+
+    ejecutar: async (
+        sock,
+        mensaje,
+        args
+    ) => {
+
+        const equipo =
+            args.join(" ")
+                .replaceAll("_", " ")
+
+        if (!equipo) {
+
+            return await sock.sendMessage(
+                mensaje.key.remoteJid,
+                {
+                    text:
+`❌ Uso correcto:
+
+/roster Titans_Esports`
+                }
+            )
+        }
+
+        const liga =
+            cargarJSON(
+                "./data/liga.json",
+                {
+                    jugadores: {}
+                }
+            )
+
+        const jugadoresEquipo =
+            Object.entries(
+                liga.jugadores
+            ).filter(
+                ([_, datos]) =>
+                    datos.equipo === equipo
+            )
+
+        if (
+            jugadoresEquipo.length === 0
+        ) {
+
+            return await sock.sendMessage(
+                mensaje.key.remoteJid,
+                {
+                    text:
+                        `📭 No hay jugadores registrados para ${equipo}.`
+                }
+            )
+        }
+
+        let texto =
+`🏆 ${equipo.toUpperCase()}
+
+`
+
+        jugadoresEquipo.forEach(
+            ([nombre]) => {
+
+                texto +=
+`👤 ${nombre}\n`
+            }
+        )
+
+        texto +=
+`\n👥 Total jugadores: ${jugadoresEquipo.length}`
+
+        await sock.sendMessage(
+            mensaje.key.remoteJid,
+            {
+                text: texto
+            }
+        )
+    }
+}
